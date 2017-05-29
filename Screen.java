ac410ff97e9342fc587beaf644168a6357b74b9a
mattang687 <mattang687@gmail.com>
2017-05-29 19:59:43 -0400

    Update Screen.java
    DO NOT COMMIT. DOES NOT WORK. Contains my efforts to search for and add questions from the bank of any type or difficulty

--- Screen.java
+++ Screen.java
@@ -19,34 +19,35 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-
+import java.util.List;
+import java.util.ArrayList;
 public class Screen extends JFrame{
-	
-	JPanel menu;
-	JComboBox [] boxes = new JComboBox[3];
-	JTextArea text;
-	Color bgColor = Color.decode("#44C5F4");
-	
-	private String [] questionChoices = {"Multiple Choice", "Fill in the Blank", "Objective Based"};
-	private String [] difficulties = {"1","2","3","4","5"};
-	private String [] amounts = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25", "26","27","28","29","30"};
-	public String[] output = new String[3];
-	
-	public Screen(){
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setBackground(Color.BLACK);
-		Toolkit tool = Toolkit.getDefaultToolkit();
-		this.setSize((int)tool.getScreenSize().getWidth(),(int)tool.getScreenSize().getHeight());
+    
+    JPanel menu;
+    JComboBox [] boxes = new JComboBox[3];
+    JTextArea text;
+    Color bgColor = Color.decode("#44C5F4");
+    
+    private String [] questionChoices = {"Multiple Choice", "Fill in the Blank", "Objective Based"};
+    private String [] difficulties = {"1","2","3","4","5"};
+    private String [] amounts = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25", "26","27","28","29","30"};
+    public String[] output = new String[3];
+    
+    public Screen(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBackground(Color.BLACK);
+        Toolkit tool = Toolkit.getDefaultToolkit();
+        this.setSize((int)tool.getScreenSize().getWidth(),(int)tool.getScreenSize().getHeight());
         setLocationRelativeTo(null);
         setResizable(true);
         
 
         initiate();
         setVisible(true);
-	}
-	
-	public void initiate(){
-		menu = new JPanel();
+    }
+    
+    public void initiate(){
+        menu = new JPanel();
         menu.setLayout(new BorderLayout());
         menu.setBackground(bgColor);
         
@@ -74,74 +75,95 @@ public class Screen extends JFrame{
         JButton add = new JButton("Add");
         add.setPreferredSize(new Dimension(150,150));
         add.addActionListener(new ActionListener() { 
-        	  public void actionPerformed(ActionEvent e) { 
-        	    addButtonPressed();
-        	  } 
-        	} );
+              public void actionPerformed(ActionEvent e) { 
+                addButtonPressed();
+              } 
+            } );
         west.add(add);
         
         JLabel choiceLabel = new JLabel("Type Of Question");
         choiceLabel.setVerticalAlignment(JLabel.BOTTOM);
         west.add(choiceLabel);
-		boxes[0] = new JComboBox(questionChoices);
-		west.add(boxes[0]);
-		
-		JLabel difficultyLabel = new JLabel("Difficulty of Question");
-		difficultyLabel.setVerticalAlignment(JLabel.BOTTOM);
-		west.add(difficultyLabel);
-		boxes[1] = new JComboBox(difficulties);
-		west.add(boxes[1]);
-		
-		JLabel amountLabel = new JLabel("Number of Question");
-		amountLabel.setVerticalAlignment(JLabel.BOTTOM);
-		west.add(amountLabel);
-		boxes[2] = new JComboBox(amounts);
-		west.add(boxes[2]);
+        boxes[0] = new JComboBox(questionChoices);
+        west.add(boxes[0]);
+        
+        JLabel difficultyLabel = new JLabel("Difficulty of Question");
+        difficultyLabel.setVerticalAlignment(JLabel.BOTTOM);
+        west.add(difficultyLabel);
+        boxes[1] = new JComboBox(difficulties);
+        west.add(boxes[1]);
+        
+        JLabel amountLabel = new JLabel("Number of Question");
+        amountLabel.setVerticalAlignment(JLabel.BOTTOM);
+        west.add(amountLabel);
+        boxes[2] = new JComboBox(amounts);
+        west.add(boxes[2]);
         
         
-		// Set East Panel
-		text = new JTextArea();
-		text.setFont(text.getFont().deriveFont(15f));
-		JScrollPane scrollPane = new JScrollPane(text);
-		scrollPane.setPreferredSize(new Dimension(getWidth()*7/10,getHeight()-200));
-		
-		east.setBorder(BorderFactory.createEmptyBorder(getHeight()/20, 0, getHeight()/10, getWidth()/20));
-		west.setBorder(BorderFactory.createEmptyBorder(getHeight()/20, getWidth()/20, getHeight()/10, getWidth()/20));
-		
-		east.add(scrollPane);
-		
-		
-		menu.add(east, BorderLayout.EAST);
-		menu.add(west, BorderLayout.WEST);
-		menu.add(north, BorderLayout.NORTH);
-		
+        // Set East Panel
+        text = new JTextArea();
+        text.setFont(text.getFont().deriveFont(15f));
+        JScrollPane scrollPane = new JScrollPane(text);
+        scrollPane.setPreferredSize(new Dimension(getWidth()*7/10,getHeight()-200));
+        
+        east.setBorder(BorderFactory.createEmptyBorder(getHeight()/20, 0, getHeight()/10, getWidth()/20));
+        west.setBorder(BorderFactory.createEmptyBorder(getHeight()/20, getWidth()/20, getHeight()/10, getWidth()/20));
+        
+        east.add(scrollPane);
+        
+        
+        menu.add(east, BorderLayout.EAST);
+        menu.add(west, BorderLayout.WEST);
+        menu.add(north, BorderLayout.NORTH);
+        
         add(menu);
         
-	}
-	
-	public void addButtonPressed(){
-		text.append(boxes[0].getSelectedItem().toString() + '\n');
-		text.append(boxes[1].getSelectedItem().toString() + '\n');
-		text.append(boxes[2].getSelectedItem().toString() + '\n');
-		output[0] = boxes[0].getSelectedItem().toString();
-		output[1] = boxes[1].getSelectedItem().toString();
-		output[2] = boxes[2].getSelectedItem().toString();
-		text.append(QuestionBank.qBank.get(0).getQuestionText());
-	}
-	
-	
-	public void waitFor(int i ){
-		
-			try {
-				TimeUnit.MICROSECONDS.sleep(i);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		
-	}
-	public static void main(String [] args){
-		Screen screen = new Screen();
-		QuestionBank questionbank = new QuestionBank();
-	}
+    }
+    
+    public void addButtonPressed(){
+        text.append(boxes[0].getSelectedItem().toString() + '\n');
+        text.append(boxes[1].getSelectedItem().toString() + '\n');
+        text.append(boxes[2].getSelectedItem().toString() + '\n');
+        
+        if(boxes[0].getSelectedItem().equals("Multiple Choice"))
+            output[0] = "0";
+        else if(boxes[0].getSelectedItem().equals("Fill in the Blank"))
+            output[0] = "1";
+        else if(boxes[0].getSelectedItem().equals("Objective Based"))
+            output[0] = "2";
+        output[1] = boxes[1].getSelectedItem().toString();
+        output[2] = boxes[2].getSelectedItem().toString();
+        
+        
+        ArrayList<Question> selected = new ArrayList<Question>();
+        selected = QuestionBank.qBank;
+        int n = 0;
+        while (n < selected.size())
+        {
+            if(selected.get(n).getDifficulty() != Integer.parseInt(output[1]))
+                selected.remove(n);
+            else if(selected.get(n).getQuestionType() != Integer.parseInt(output[0]))
+                selected.remove(n);
+            
+        }
+        for (n = 0; n < Integer.parseInt(output[2]); n++)
+            text.append(selected.get(n).getQuestionText());
+        
+    }
+    
+    
+    public void waitFor(int i ){
+        
+            try {
+                TimeUnit.MICROSECONDS.sleep(i);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        
+    }
+    public static void main(String [] args){
+        Screen screen = new Screen();
+        QuestionBank questionbank = new QuestionBank();
+    }
 }
